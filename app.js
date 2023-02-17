c8971dfc94132809c4e74ca7599e9f38894ca489
Jimuel Madlangsakay <jmuelkoh22@gmail.com>
2023-02-17 19:54:08 +0800

    update

--- app.js
+++ app.js
@@ -134,3 +134,43 @@ if(isNaN(age1) || age1 < 0){
     console.log("You can do what ever you want.");
 }
 
+
+const h1 = document.querySelector("div.hello:first-child h1");
+
+console.dir(h1);
+
+function handleh1Click(){
+    h1.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "Tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffline(){
+    alert("S0S no WIFI");
+}
+
+function handleWindowOnline(){
+    alert("All good");
+}
+h1.addEventListener("click", handleh1Click);
+h1.addEventListener("mouseenter", handleMouseEnter)
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
